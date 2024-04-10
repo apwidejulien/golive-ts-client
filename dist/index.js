cae566da2daf070e9dceb87bda3a41b8536a1903
Julien Repond <julien.repond@gmail.com>
2024-04-10 23:26:49 +0200

    updated iport

--- dist/index.js
+++ dist/index.js
@@ -27490,6 +27490,29 @@ exports.WatcherService = WatcherService;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -27499,30 +27522,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
+const core = __importStar(__nccwpck_require__(2186));
 const client_1 = __nccwpck_require__(7929);
 function log(message) {
-    core_1.default.info(message);
+    core.info(message);
 }
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const apiToken = core_1.default.getInput('apiToken');
-            const baseUrl = core_1.default.getInput('baseUrl');
+            const apiToken = core.getInput('apiToken');
+            const baseUrl = core.getInput('baseUrl');
             client_1.OpenAPI.BASE = baseUrl;
             client_1.OpenAPI.TOKEN = apiToken;
             // github.context.
             const apps = yield client_1.ApplicationService.getApplications({ expand: false });
-            core_1.default.setOutput('status', 'Success');
+            core.setOutput('status', 'Success');
             log(`apps count: ${apps.length}`);
         }
         catch (error) {
             if (error instanceof Error) {
-                core_1.default.setFailed(error.message);
+                core.setFailed(error.message);
             }
         }
     });
