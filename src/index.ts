2a813eaf9820a2e64a4839a1b5ce9b080d7a0ab4
Julien Repond <julien.repond@gmail.com>
2024-04-20 21:51:02 +0200

    test new github api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {ApplicationService, OpenAPI} from "./client";
+// https://www.typescriptlang.org/docs/handbook/modules/appendices/esm-cjs-interop.html
+import {ApplicationService, OpenAPI} from "./client"
+//const {restEndpointMethods} = await import('@octokit/plugin-rest-endpoint-methods')
+// import {restEndpointMethods} from '@octokit/plugin-rest-endpoint-methods'
+const plugin = require('@octokit/plugin-rest-endpoint-methods')
 
 function log(message: string) {
     core.info(message)
@@ -8,14 +12,37 @@ function log(message: string) {
 
 async function run() {
     try {
+
+        // const GoliveOctokit = Octokit.plugin(restEndpointMethods)
+        // const octokit = new GoliveOctokit()
         const apiToken = core.getInput('apiToken')
         const baseUrl = core.getInput('baseUrl')
         const githubToken = core.getInput('githubToken')
+        // https://gist.github.com/slavafomin/cd7a54035eff5dc1c7c2eff096b23b6b
+        // https://github.com/actions/toolkit/issues/1555
+        // https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
+        // https://www.typescriptlang.org/docs/handbook/modules/reference.html#packagejson-exports
         // https://docs.github.com/en/actions/security-guides/automatic-token-authentication
         // https://github.com/octokit/auth-action.js/?tab=readme-ov-file
+        // https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/making-authenticated-api-requests-with-a-github-app-in-a-github-actions-workflow
         //core.getInput("github")
         const token = core.getInput('GITHUB_TOKEN')
-        // const gc = github.getOctokit(token)
+        const context = github.context
+
+        // const MyOctokit = Octokit.plugin<Octokit & Constructor<Api>>(restEndpointMethods);
+        // const m = new MyOctokit({})
+        // const octokit = github.getOctokit<Octokit & Api>(githubToken, undefined, restEndpointMethods)
+        const api = plugin.restEndpointMethods(github.getOctokit(githubToken))
+        // const api: Api = github.getOctokit<Api>(githubToken, undefined, plugin.restEndpointMethods)
+        const response = await api.rest.actions.listWorkflowRunsForRepo({
+            ...context.repo
+        })
+        log(`runs count: ${response.data.workflow_runs.length}`)
+        /*
+        const response: GetWorkflowRunsResponse = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
+            ...context
+        })
+        */
         log(`
             token: ${token}
             githubToken: ${githubToken}
