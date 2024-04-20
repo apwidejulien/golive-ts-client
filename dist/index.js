ab85129c5a5def4072c8696916e57c058c43c38c
Julien Repond <julien.repond@gmail.com>
2024-04-20 13:19:22 +0200

    try to get token

--- dist/index.js
+++ dist/index.js
@@ -31806,9 +31806,13 @@ function run() {
         try {
             const apiToken = core.getInput('apiToken');
             const baseUrl = core.getInput('baseUrl');
+            // https://docs.github.com/en/actions/security-guides/automatic-token-authentication
+            // https://github.com/octokit/auth-action.js/?tab=readme-ov-file
             //core.getInput("github")
-            // github.getOctokit()
+            const token = core.getInput('GITHUB_TOKEN');
+            // const gc = github.getOctokit(token)
             log(`
+            token: ${token}
             workflow: ${github.context.workflow}
             repo: ${github.context.repo.repo}
             runId: ${github.context.runId}
